2e723ac03b2fe94ecfaa93d1c48b4d386d5747e2
xiaobai <huibo5263285@163.com>
2013-04-09 18:07:52 +0800

    1.0

--- js/index.js
+++ js/index.js
@@ -5,3 +5,25 @@
  * Time: 上午9:32
  * To change this template use File | Settings | File Templates.
  */
+(function () {
+    String.prototype.formatter = function (obj) {
+        var result = this;
+        if (typeof obj === 'object' && arguments.length === 1) {
+            alert(1);
+            for (var key in obj) {
+                var reg = new RegExp("({" + key + "})" , 'g');
+                result.replace(reg, obj[key]);
+            }
+        } else {
+            for (var i = 0, l = arguments.length; i < l; i++) {
+                result.replace('{' + i + '}', arguments[i]);
+            }
+        }
+        return String(result);
+    };
+
+    var str = "abc{name},{id}";
+
+    console.log(str.formatter({name:"xiaobai","id":"123"}));
+
+})();
